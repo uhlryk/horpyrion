9f17e2f0f003bdf3b2753ccdb9cd68d5f7df9e28
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2019-03-16 22:53:07 +0100

    root user schema records tests

---
+++ src/Horpyrion.root.userSchema.record.spec.js
@@ -0,0 +1,56 @@
+import Horpyrion from "./Horpyrion";
+
+describe("Horpyrion root user and user schema context and record context", () => {
+    let horpyrion;
+    let USER_ID;
+    beforeEach(() => {
+        horpyrion = new Horpyrion(DB_CONFIGURATION);
+        return horpyrion
+            .sync({ force: true })
+            .then(() => {
+                return horpyrion
+                    .setRootUser()
+                    .setUserSchema()
+                    .createRecord("SOME_USER");
+            })
+            .then(user => {
+                USER_ID = user.id;
+            });
+    });
+
+    it("should return record data", () => {
+        return horpyrion
+            .setUser()
+            .setUserSchema()
+            .setUserRecord(USER_ID)
+            .getData()
+            .then(resp => {
+                expect(resp).to.containSubset({
+                    id: expectedValue => expectedValue,
+                    name: "SOME_USER"
+                });
+            });
+    });
+
+    it("should update user record", () => {
+        return horpyrion
+            .setRootUser()
+            .setUserSchema()
+            .setUserRecord(USER_ID)
+            .updateRecord({ name: "SOME_USER" })
+            .then(resp => {
+                expect(resp).to.be.true();
+            });
+    });
+
+    it("should remove user record", () => {
+        return horpyrion
+            .setRootUser()
+            .setUserSchema()
+            .setUserRecord(USER_ID)
+            .removeRecord()
+            .then(resp => {
+                expect(resp).to.be.true();
+            });
+    });
+});
